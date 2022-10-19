b5be115bbb9ec6a05e28219b082ca4147e554fe5
lawrence-laz <laurynas@lazauskas.info>
2022-10-19 23:07:03 +0300

    add solution for 2021 2 day part 1

---
+++ 2021/2.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+var input = File
+    .ReadAllLines("input.txt")
+    .Select(line => (
+        Direction: line.Split(' ')[0],
+        Units: int.Parse(line.Split(' ')[1], CultureInfo.InvariantCulture)))
+    .ToList();
+
+var horizontal = 0;
+var depth = 0;
+
+input.ForEach(command =>
+{
+    horizontal += command switch
+    {
+        { Direction: "forward" } => command.Units,
+        _ => 0
+    };
+
+    depth += command switch
+    {
+        { Direction: "up" } => -command.Units,
+        { Direction: "down" } => command.Units,
+        _ => 0
+    };
+});
+
+System.Console.WriteLine(horizontal * depth);
