5911cb33d5e7965ea17114c69bd1257e09b10c37
lawrence-laz <laurynas@lazauskas.info>
2022-10-19 22:55:41 +0300

    add solutions for 2021 1 day

---
+++ 2021/1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+var input = File
+    .ReadAllLines("input.txt")
+    .Select(line => int.Parse(line, CultureInfo.InvariantCulture));
+
+var output = input
+    .Zip(input.Skip(1))
+    .Count(items => items.First < items.Second);
+
+System.Console.WriteLine(output);
+
---
+++ 2021/1.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+var input = File
+    .ReadAllLines("input.txt")
+    .Select(line => int.Parse(line, CultureInfo.InvariantCulture));
+
+var sums = input
+    .Zip(input.Skip(1), input.Skip(2))
+    .Select(items => items.First + items.Second + items.Third);
+
+var output = sums
+    .Zip(sums.Skip(1))
+    .Count(items => items.First < items.Second);
+
+System.Console.WriteLine(output);
+
