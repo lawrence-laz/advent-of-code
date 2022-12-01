465a38f1a0dd05aa4d92166642979d166f31ad0f
lawrence-laz <laurynas@lazauskas.info>
2022-12-01 19:34:16 +0200

    add solutions for 2022 1 day

---
+++ 2022/1.1/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine(
+    System.IO.File
+        .ReadAllText("input.txt")
+        .Split("\n\n")
+        .Select(groupLines => groupLines
+            .Split("\n")
+            .Where(line => !line.IsNullOrEmpty())
+            .Select(int.Parse)
+            .Sum())
+        .Max()
+);
---
+++ 2022/1.2/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine(
+    System.IO.File
+        .ReadAllText("input.txt")
+        .Split("\n\n")
+        .Select(groupLines => groupLines
+            .Split("\n")
+            .Where(line => !line.IsNullOrEmpty())
+            .Select(int.Parse)
+            .Sum())
+        .OrderByDescending()
+        .Take(3)
+        .Sum()
+);
