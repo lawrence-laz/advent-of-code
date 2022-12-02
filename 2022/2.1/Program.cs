c4d12b4f6f6d86bcfa049b387f8c26a032288e10
lawrence-laz <laurynas@lazauskas.info>
2022-12-02 18:47:13 +0200

    add solutions for 2022 2 day

---
+++ 2022/2.1/Program.cs
@@ -0,0 +1,18 @@
+﻿System.Console.WriteLine(
+    System.IO.File
+        .ReadAllLines("input.txt")
+        .Select(line => (Opponent: line.Split(" ")[0], Me: line.Split(" ")[1]))
+        .Select(game => (game.Opponent, game.Me) switch
+        {
+            (_, "X") => 1,
+            (_, "Y") => 2,
+            (_, "Z") => 3
+        } + (game.Opponent, game.Me) switch
+        {
+            ("A", "X") or ("B", "Y") or ("C", "Z") => 3,
+            ("C", "X") or ("A", "Y") or ("B", "Z") => 6,
+            _ => 0
+        })
+        .Sum()
+);
+
---
+++ 2022/2.2/Program.cs
@@ -0,0 +1,28 @@
+﻿System.Console.WriteLine(
+    System.IO.File
+        .ReadAllLines("input.txt")
+        .Select(line => (Opponent: line.Split(" ")[0], Outcome: line.Split(" ")[1]))
+        .Select(game => (Outcome: game.Outcome, Me: (game.Opponent, game.Outcome) switch
+        {
+            ("A", "X") => "C",
+            ("B", "X") => "A",
+            ("C", "X") => "B",
+            (_, "Y") => game.Opponent,
+            ("A", "Z") => "B",
+            ("B", "Z") => "C",
+            ("C", "Z") => "A",
+        }))
+        .Select(game => game.Me switch
+        {
+            "A" => 1,
+            "B" => 2,
+            "C" => 3
+        } + game.Outcome switch
+        {
+            "X" => 0,
+            "Y" => 3,
+            "Z" => 6,
+        })
+        .Sum()
+);
+
