7dd88d3580fbf8e693fe19f0f07a6192f2e3f595
lawrence-laz <laurynas@lazauskas.info>
2022-12-03 22:13:08 +0200

    add solutions for 2022 3 day

---
+++ 2022/3.1/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine(
+    System.IO.File
+        .ReadAllLines("input.txt")
+        .SelectMany(line => line.Take(line.Length / 2).Intersect(line.Skip(line.Length / 2)))
+        .Select(character => char.IsUpper(character)
+            ? character - 0x40 + 26 // First char before 'A' in ASCII
+            : character - 0x60) // First char before 'a' in ASCII
+        .Sum()
+);
+
---
+++ 2022/3.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using MoreLinq;
+
+System.Console.WriteLine(
+    System.IO.File
+        .ReadAllLines("input.txt")
+        .Batch(3)
+        .SelectMany(groupOfThree => groupOfThree.ElementAt(0)
+            .Intersect(groupOfThree.ElementAt(1))
+            .Intersect(groupOfThree.ElementAt(2)))
+        .Select(character => char.IsUpper(character)
+            ? character - 0x40 + 26 // First char before 'A' in ASCII
+            : character - 0x60) // First char before 'a' in ASCII
+        .Sum()
+);
+
