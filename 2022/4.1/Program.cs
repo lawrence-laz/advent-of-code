65fd5f783165ff1cbe53163574ea46d7800abe80
lawrence-laz <laurynas@lazauskas.info>
2022-12-04 10:39:40 +0200

    add solutions for 2022 4 day

---
+++ 2022/4.1/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine(
+    System.IO.File
+        .ReadAllLines("input.txt")
+        .Select(line => line.Split(","))
+        .Select(pair => (First: pair[0].Split('-'), Second: pair[1].Split('-')))
+        .Select(pair => (
+            First: (RangeStart: int.Parse(pair.First[0]), RangeEnd: int.Parse(pair.First[1])),
+            Second: (RangeStart: int.Parse(pair.Second[0]), RangeEnd: int.Parse(pair.Second[1]))
+        ))
+        .Select(pair => pair.First.RangeStart <= pair.Second.RangeStart && pair.First.RangeEnd >= pair.Second.RangeEnd
+            ? 1
+            : pair.Second.RangeStart <= pair.First.RangeStart && pair.Second.RangeEnd >= pair.First.RangeEnd
+            ? 1
+            : 0)
+        .Sum()
+);
+
---
+++ 2022/4.2/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.WriteLine(
+    System.IO.File
+        .ReadAllLines("input.txt")
+        .Select(line => line.Split(","))
+        .Select(pair => (First: pair[0].Split('-'), Second: pair[1].Split('-')))
+        .Select(pair => (
+            First: (RangeStart: int.Parse(pair.First[0]), RangeEnd: int.Parse(pair.First[1])),
+            Second: (RangeStart: int.Parse(pair.Second[0]), RangeEnd: int.Parse(pair.Second[1]))
+        ))
+        .Select(pair => Enumerable.Range(pair.First.RangeStart, pair.First.RangeEnd - pair.First.RangeStart + 1)
+            .Intersect(Enumerable.Range(pair.Second.RangeStart, pair.Second.RangeEnd - pair.Second.RangeStart + 1))
+            .Any() ? 1 : 0)
+        .Sum()
+);
+
