0ba7a95140fce09fec7d57b64623683d581e0714
lawrence-laz <laurynas@lazauskas.info>
2022-12-04 11:58:38 +0200

    refactor solution for 2022 4 day

--- 2022/4.1/Program.cs
+++ 2022/4.1/Program.cs
@@ -3,15 +3,8 @@
         .ReadAllLines("input.txt")
         .Select(line => line.Split(","))
         .Select(pair => (First: pair[0].Split('-'), Second: pair[1].Split('-')))
-        .Select(pair => (
-            First: (RangeStart: int.Parse(pair.First[0]), RangeEnd: int.Parse(pair.First[1])),
-            Second: (RangeStart: int.Parse(pair.Second[0]), RangeEnd: int.Parse(pair.Second[1]))
-        ))
-        .Select(pair => pair.First.RangeStart <= pair.Second.RangeStart && pair.First.RangeEnd >= pair.Second.RangeEnd
-            ? 1
-            : pair.Second.RangeStart <= pair.First.RangeStart && pair.Second.RangeEnd >= pair.First.RangeEnd
-            ? 1
-            : 0)
-        .Sum()
+        .Select(pair => (pair.First[0].ParseInt()..pair.First[1].ParseInt())
+            .ContainsOrIsContainedBy(pair.Second[0].ParseInt()..pair.Second[1].ParseInt()))
+        .Count(isContained => isContained is true)
 );
 
