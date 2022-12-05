f3c7a0ee92ad41207786d5d1e7391a99c9505f08
lawrence-laz <laurynas@lazauskas.info>
2022-12-05 22:37:42 +0200

    add solution for 2022 5 day part 1

---
+++ 2022/5.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using MoreLinq;
+
+System.Console.WriteLine(
+    System.IO.File
+        .ReadAllLines("input.txt")
+        .Split("")
+        .Lead(1, (firstHalf, secondHalf) => (
+            Stacks: firstHalf
+                .Reverse()
+                .Skip(1)
+                .Select(line => line
+                    .Batch(4)
+                    .Select(crate => string.Concat(crate.Except(new[] { '[', ']', ' ' }))))
+                .Transpose()
+                .Select(column => column
+                    .Where(crate => !string.IsNullOrWhiteSpace(crate))
+                    .Aggregate(new Stack<string>(), (stack, crate) => { stack.Push(crate); return stack; }))
+                .ToList(),
+            Operations: secondHalf?
+                .Select(line => (
+                    Count: line.Split(' ')[1].ParseInt(),
+                    FromStack: line.Split(' ')[3].ParseInt() - 1,
+                    ToStack: line.Split(' ')[5].ParseInt() - 1))))
+        .Take(1)
+        .Select(input => input.Operations
+            .ToList()
+            .Aggregate(input.Stacks, (stacks, operation) => Enumerable
+                .Range(0, operation.Count)
+                .Aggregate(stacks, (_, _) => { stacks[operation.ToStack].Push(stacks[operation.FromStack].Pop()); return stacks; })))
+        .First()
+        .Aggregate("", (result, stack) => result + stack.Peek())
+);
+
