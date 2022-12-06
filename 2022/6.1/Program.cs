52e6ec358f3c0c5b85781412284695363884bac9
lawrence-laz <laurynas@lazauskas.info>
2022-12-06 19:07:54 +0200

    add solutions for 2022 6 day

---
+++ 2022/6.1/Program.cs
@@ -0,0 +1,9 @@
+﻿using MoreLinq;
+
+Console.WriteLine(
+    File.ReadAllText("input.txt")
+        .Window(4)
+        .TakeUntil(lastFour => lastFour.Distinct().Count() == 4)
+        .Count() + 3
+);
+
---
+++ 2022/6.2/Program.cs
@@ -0,0 +1,9 @@
+﻿using MoreLinq;
+
+Console.WriteLine(
+    File.ReadAllText("input.txt")
+        .Window(14)
+        .TakeUntil(lastFour => lastFour.Distinct().Count() == 14)
+        .Count() + 13
+);
+
