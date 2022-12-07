f7cf6b2561e7173a2bd04a79a59a45bb56cca09f
lawrence-laz <laurynas@lazauskas.info>
2022-12-07 21:39:48 +0200

    add solution for 2022 7 day part 1

---
+++ 2022/7.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using MoreLinq;
+using static MoreLinq.MoreEnumerable;
+
+Console.WriteLine(
+    File.ReadAllLines("input.txt")
+        .Skip(1)
+        .Where(line => !line.EndsWith("ls"))
+        .Select(line => line.TrimStart("$ ").Split(' '))
+        .Select(tokens =>
+            (
+                ChangeDirectoryTo: tokens[0] == "cd" ? tokens[1] : ".",
+                FileSize: int.TryParse(tokens[0], out var size) ? size : 0
+            ))
+        .Aggregate(
+            (Stack: new Stack<string>(), CurrentPath: "/", Sizes: new Dictionary<string, int>()),
+            (state, command) =>
+            {
+                if (command.ChangeDirectoryTo == "..")
+                {
+                    state.Stack.Pop();
+                }
+                else if (command.ChangeDirectoryTo != ".")
+                {
+                    state.CurrentPath = Path.GetFullPath(Path.Combine(state.CurrentPath, command.ChangeDirectoryTo));
+                    state.Stack.Push(state.CurrentPath);
+                }
+                else
+                {
+                    state.Stack.ToList().ForEach(path => state.Sizes[path] = state.Sizes.GetOrAdd(path, () => 0) + command.FileSize);
+                }
+
+                return state;
+            })
+        .Sizes.Values
+        .Where(size => size <= 100000)
+        .Sum()
+);
+
