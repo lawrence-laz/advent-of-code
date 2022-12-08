a41d3137e3438096247876694568c7300fc0e317
lawrence-laz <laurynas@lazauskas.info>
2022-12-08 22:44:34 +0200

    add solution for 2022 8 day part 1

---
+++ 2022/8.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using MoreLinq;
+
+var id = 1;
+var rows = File
+    .ReadAllLines("input.txt")
+    .Select(line => line
+        .Select(character => (Height: int.Parse(character.ToString()), Id: id++))
+        .ToArray())
+    .ToArray();
+var visibleTrees = new HashSet<int>();
+
+rows.ForEach(row => RayCastForVisibleTrees(row)); // From left
+rows.ForEach(row => RayCastForVisibleTrees(row.Reverse())); // From right
+rows.Transpose().ForEach(row => RayCastForVisibleTrees(row)); // From top
+rows.Transpose().ForEach(row => RayCastForVisibleTrees(row.Reverse())); // From bottom
+
+void RayCastForVisibleTrees(IEnumerable<(int Height, int Id)> row)
+{
+    var highestTreeHeight = -1;
+    row.ForEach(tree =>
+    {
+        if (tree.Height > highestTreeHeight)
+        {
+            visibleTrees.Add(tree.Id);
+        }
+        highestTreeHeight = Math.Max(highestTreeHeight, tree.Height);
+    });
+}
+
+Console.WriteLine(visibleTrees.Count());
